1d0c605230a39dacb0c50c1fbbee88f4ea4e1a6c
varshadhanotiya <77069458+varshadhanotiya@users.noreply.github.com>
2025-04-19 16:26:15 +0530

    Hardcoding uri

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   console.log(process.env.NODE_ENV);
   console.log(process.env.PORT);
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect('mongodb+srv://varshadhanotiya7026:admin@cluster0.qi1l7.mongodb.net/api_deployment?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
