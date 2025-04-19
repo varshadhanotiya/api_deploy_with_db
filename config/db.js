d808cc8c1628a5fb8f0fae95403e23802a549914
varshadhanotiya <77069458+varshadhanotiya@users.noreply.github.com>
2025-04-19 16:19:58 +0530

    adding logs

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  console.log(process.env.MONGO_URI);
+  console.log(process.env.NODE_ENV);
+  console.log(process.env.PORT);
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
